e2be49dd7002c43648bdb75b9250477a9ebe73a0
Jason Boerner <jasonboerner@Jasons-MacBook-Pro.local>
2019-03-18 09:01:33 -0400

    changes line spacing in popup

--- scripts/index.js
+++ scripts/index.js
@@ -107,10 +107,9 @@ function success(pos) {
         let breweryLocation = [breweryLng, breweryLat]
         let marker = new mapboxgl.Marker()
             .setLngLat(breweryLocation)
-            .setPopup(new mapboxgl.Popup({offset:20})
-            // how to add a space between city and state?
+            .setPopup(new mapboxgl.Popup({offset:5})
             // how to make website into a clickable link?
-            .setHTML('<h3>' + breweryObject.name + '</h3>' + breweryObject.street + '<p>' + breweryObject.city + breweryObject.state +'</p>' + '<p>' + '<a' + 'href=breweryObject.website_url' + '>' + breweryObject.website_url + '</a>' + '</p>'))
+            .setHTML('<h3>' + breweryObject.name + '</h3>' + breweryObject.street + '<p>' + breweryObject.city + breweryObject.state +'</p>' + breweryObject.website_url + '</p>'))
             .addTo(map);
         }
 
